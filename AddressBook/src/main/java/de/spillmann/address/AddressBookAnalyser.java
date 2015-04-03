8974cda541a05362889dc3b88c4cd8617334462e
spillmann <mail@martin-spillmann.de>
2015-04-03 19:54:12 +0200

    s

--- AddressBook/src/main/java/de/spillmann/address/AddressBookAnalyser.java
+++ AddressBook/src/main/java/de/spillmann/address/AddressBookAnalyser.java
@@ -27,7 +27,7 @@ public class AddressBookAnalyser {
 	private static Logger logger = Logger.getLogger(AddressBookAnalyser.class.toString());
 	
 	/**
-	 * ich bin ein test 123_4_567_78
+	 * ich bin ein test 123_4_567_78_99
 	 */
 	
 	/**
